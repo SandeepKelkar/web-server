1015ba40889c7c7cd8caa912c29a5c4994f97565
Sandeep Kelkar <sandeepkelkar67@gmail.com>
2016-01-03 16:04:05 +0530

    Update language

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var middleWare = {
 app.use(middleWare.logger);
 
 app.get('/about', middleWare.requireAuthentication,  function (req, res) {
-	res.send('About us Page');
+	res.send('About us Page!');
 });
 
 app.use(express.static(__dirname + '/public'));
